41928fd8086441d063eaa5ae46f166bdd0231fa8
Dieppois <83698803+Dieppois@users.noreply.github.com>
2021-06-08 15:22:27 +0200

    Update E_3_7.java

--- Project_Java/Algorithme Exercices/src/E_3_7.java
+++ Project_Java/Algorithme Exercices/src/E_3_7.java
@@ -10,7 +10,8 @@ Les prénoms sont classés par ordre alphabétique
 On connaît le nombre d'éléments de la table
 Algorithme:
 On partitionne la table en 2 sous-tables et un élément médian, et, suivant le résultat de la comparaison de l'élément 
-médian et du prénom recherché (plus grand, plus petit ou égal) on recommence la recherche sur une des 2 soustables, jusqu'à avoir trouvé ou obtenir une sous-table vide (le prénom est alors absent de la table).
+médian et du prénom recherché (plus grand, plus petit ou égal) on recommence la recherche sur une des 2 soustables, 
+jusqu'à avoir trouvé ou obtenir une sous-table vide (le prénom est alors absent de la table).
 On cherche 'olga' dans la table:
 Milieu: élément 4
 'olga'>'cunégonde' -> 'olga' est entre 4 et 7
@@ -24,7 +25,96 @@ public class E_3_7 {
 
 	public static void main(String[] args) {
 		
+	Scanner sc = new Scanner (System.in);
+	
+	String [][] monTab = {{"Agathe", "1"},
+						{"Berthe","2"},
+						{"Chloe", "3"},
+						{"Cunegonde", "4"},
+						{"Olga","5"},
+						{"Raymonde","6"},
+						{"Sidonie","7"}};
+	
+	int tab[] = {1, 2, 3, 4, 5, 6, 7};
+
+	trierEffectif (monTab, tab);
+	
+	
+	
+	System.out.println("entrez nombre de 1 a 7");
+	int val = sc.nextInt();
+	int l = tab.length-1;
+	int f = 0;   
+	
+	
+	binarySearch(tab,f,l,val);  
+	
+
+	System.out.println(monTab[val-1][0]+" est dans le tableau, emplacement : "+(monTab[val-1][1]));
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	public static void trierEffectif(String[][]_tabf2, int [] tempTab) {
+		
+		tempTab = new int [_tabf2.length];
+
+		for (int i = 0; i < _tabf2.length; i++) {
+				tempTab[i] = Integer.parseInt(_tabf2[i][1]); }
+		
+			String temp2 = "";
+			int temp = 0;
+
+			for (int i = 0; i < tempTab.length-1; i++) {
+				for (int j = i+1; j < tempTab.length; j++) {
+					if (tempTab [i] >tempTab [j]) {
+						
+						temp = tempTab [i];
+						tempTab [i] = tempTab [j];
+						tempTab [j] = temp;
+						
+						temp2 = _tabf2[i][1];
+                        _tabf2[i][1] = _tabf2[j][1];
+                        _tabf2[j][1] = temp2;
+                     
+                        temp2 = _tabf2[i][0];
+                        _tabf2[i][0] = _tabf2[j][0];
+                        _tabf2[j][0] = temp2;
+	
+	
+	
+					}
+
+				}
+			}
+	}
+		
+	  public static void binarySearch(int tab[], int f, int l, int val){
+		    int mid = (f + l)/2;                		 // définir le milieu du tableau
+		    while(f <= l){                     			 //
+		      if (tab[mid] < val){						// si val se trouve dans la partie inf du tableau
+		        f = mid + 1;   							// f 0 = moitié +1
+		      }else if(tab[mid] == val){          		//  si ta trouvé 
+		      //  System.out.println("L'élément se trouve à l'index: " + mid); 
+		        break;
+		      }else{
+		         l = mid - 1;                        	 // sinon l max = moitié -1
+		      }
+		      mid = (f + l)/2;							// mid moitié = ( 0 / moitié +1 + moitié - 1 / max ) / 2 
+		   }
+		    if (f > l){									// 
+		      System.out.println("L'élément n'existe pas!");
+		    }
+		   }
 
 	}
 
-}
+
