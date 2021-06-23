60b0700191bb5a02f9432965028b899be6ab7e45
Dieppois <83698803+Dieppois@users.noreply.github.com>
2021-06-23 07:38:46 +0200

    .

---
+++ Visual Studio_C#/Exercices Csharp/TestMajorite/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestMajorite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir votre date de naissance ! dd/mm/aaaa");
+            string chaineDateAnniv = Console.ReadLine();
+            string[] tabDateAnniv = chaineDateAnniv.Split("/");
+            DateTime dateAnniv = new DateTime(int.Parse(tabDateAnniv[2]), int.Parse(tabDateAnniv[1]), int.Parse(tabDateAnniv[0]));
+            // Console.WriteLine(dateAnniv.ToString("dd/MM/yyyy"));
+            DateTime dateJour = DateTime.Today;
+            DateTime jourMaj = new DateTime(dateJour.Year - 18, dateJour.Month, dateJour.Day);
+            if (dateAnniv.CompareTo(jourMaj) == -1)
+            {
+                Console.WriteLine("\nVous êtes majeur");
+            }
+            else if (dateAnniv.CompareTo(jourMaj) == 0)
+
+            {
+                Console.WriteLine("\nVous êtPes tout juste majeur, bon anniversaire !! ");
+            }
+            else
+
+            {
+                Console.WriteLine("\nVous êtes mineur");
+            }
+            Console.ReadKey();
+        }
+    }
+}
